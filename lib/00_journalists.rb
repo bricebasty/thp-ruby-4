c897d8a184c1800f89046a0088a00a947282f426
celgost <brice.basty1@gmail.com>
2023-07-05 13:29:09 +0200

    first commit

---
+++ lib/00_journalists.rb
@@ -0,0 +1,128 @@
+TWITTER_HANDLES = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+def user_prompt
+  print "\e[1m\e[31m> "
+  input = gets.chomp.downcase
+  puts "\e[0m"
+  return input
+end
+
+def return_to_menu
+  loop do
+    print "Veux-tu revenir au menu ? \e[1mY/N\e[0m  "
+    input = user_prompt
+    if input == "y" || input == "yes"
+      menu
+      break
+    elsif input == "n" || input =="no"
+      puts "Merci d'avoir utilisé ce petit logiciel \u{1F600}\n"
+      break
+    else
+      puts "\e[41m\e[1mEntrée invalide.\e[0m\n\n"
+    end
+  end
+end
+
+def handles_quantity
+  puts "\e[1m\e[42mIl y a #{TWITTER_HANDLES.length} handles dans l'array\e[0m\n\n"
+  return_to_menu
+end
+
+def shortest_handle
+  shortest_element = TWITTER_HANDLES.min_by { |element| element.length }
+  puts "\e[1m\e[42mHandle le plus court : #{shortest_element}\e[0m\n\n"
+  return_to_menu
+end
+
+def five_characters_handles
+  number = 0
+  TWITTER_HANDLES.each do |element|
+    if element.length == 6
+      number += 1
+    end
+  end
+  puts "\e[1m\e[42mNombres de handles contenant 5 caractères : #{number}\e[0m\n\n"
+  return_to_menu
+end
+
+def uppercase_handles
+  number = 0
+  TWITTER_HANDLES.each do |element|
+    second_character = element[1]
+    if second_character == second_character.upcase
+      number += 1
+    end
+  end
+  puts "\e[1m\e[42mNombres de handles commençant par une majuscule : #{number}\e[0m\n\n"
+  return_to_menu
+end
+
+def handles_by_az
+  sorted_handles = TWITTER_HANDLES.sort
+  puts "\e[1m\e[32m#{sorted_handles.join("\n")}\e[0m\n\n"
+  return_to_menu
+end
+
+def handles_by_size
+  sorted_handles = TWITTER_HANDLES.sort_by { |element| element.length }
+  puts "\e[1m\e[32m#{sorted_handles.join("\n")}\e[0m\n\n"
+  return_to_menu
+end
+
+def epenser_position
+  index = TWITTER_HANDLES.index("@epenser")
+  puts "\e[1m\e[42m@epenser est à la position #{index} dans l'array\e[0m\n\n"
+  return_to_menu
+end
+
+def size_repartition
+  repartition = TWITTER_HANDLES.group_by { |element| element.length }
+  sorted_repartition = repartition.sort_by { |length, _elements| length }.to_h
+
+  sorted_repartition.each do |length, elements|
+    puts "\e[1m\e[42mHandles de #{length - 1} caractères :\e[0m\e[32m #{elements.join(', ')}\e[0m\n\n"
+  end
+  return_to_menu
+end
+
+def menu
+  loop do
+    puts "\n\e[33m\e[4mChoisis une fonction à exécuter\e[0m\n\n\e[41m1\e[0m Nombre de handles dans l'array\n\e[42m2\e[0m Handle le plus court\n\e[43m3\e[0m Nombre de handles contenant 5 caractères\n\e[44m4\e[0m Handles commençant par une majuscule\n\e[45m5\e[0m Handles par ordre alphabétique\n\e[46m6\e[0m Handles par taille\n\e[41m7\e[0m Position de @epenser\n\e[42m8\e[0m Répartition des handles par taille\n\e[47mX\e[0m Quitter"
+    input = user_prompt
+    case input
+    when "1"
+      handles_quantity
+      break
+    when "2"
+      shortest_handle
+      break
+    when "3"
+      five_characters_handles
+      break
+    when "4"
+      uppercase_handles
+      break
+    when "5"
+      handles_by_az
+      break
+    when "6"
+      handles_by_size
+      break
+    when "7"
+      epenser_position
+      break
+    when "8"
+      size_repartition
+      break
+    when "x", "X"
+      break
+    else
+      puts "\e[41m\e[1mEntrée invalide.\e[0m"
+      retry_flag = true
+    end
+    break unless retry_flag
+    retry_flag = false
+  end
+end
+
+menu
